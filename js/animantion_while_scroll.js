d8baf8bd660c320f682541771197dfd7916d4982
odyyyy <iliya.odyvan4ikys@gmail.com>
2023-12-08 15:00:19 +0300

    updated

--- js/animantion_while_scroll.js
+++ js/animantion_while_scroll.js
@@ -3,19 +3,19 @@ window.addEventListener('scroll', function() {
   
     var scrollY = window.scrollY || document.documentElement.scrollTop;
   
-    if (scrollY >= 300) {
+    if (scrollY >= 500) {
       categoryElementsList[0].classList.add('fadeInFromLeft');
     } else {
       categoryElementsList[0].classList.remove('fadeInFromLeft');
     }
 
-    if (scrollY >= 1200) {
+    if (scrollY >= 1500) {
         categoryElementsList[1].classList.add('fadeInFromLeft');
       } else {
         categoryElementsList[1].classList.remove('fadeInFromLeft');
       }
 
-      if (scrollY >= 2000) {
+      if (scrollY >= 2200) {
         categoryElementsList[2].classList.add('fadeInFromLeft');
       } else {
         categoryElementsList[2].classList.remove('fadeInFromLeft');
